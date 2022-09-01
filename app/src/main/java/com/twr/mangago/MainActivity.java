3794f9dbf0f481f9c6b9709905ada121446c050e
Wen Rui <teewenrui31@gmail.com>
2022-09-01 16:11:57 +0800

    .

--- app/src/main/java/com/twr/mangago/MainActivity.java
+++ app/src/main/java/com/twr/mangago/MainActivity.java
@@ -4,23 +4,29 @@ package com.twr.mangago;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.SearchView;
-import androidx.core.view.MenuItemCompat;
-import androidx.core.view.WindowCompat;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.core.view.GestureDetectorCompat;
 import androidx.core.view.WindowInsetsCompat;
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
+
+import android.app.ActionBar;
+import android.gesture.Gesture;
 import android.os.Build;
 import android.os.Bundle;
 import android.util.Base64;
+import android.util.Log;
+import android.view.GestureDetector;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.WindowInsets;
 import android.view.WindowInsetsController;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.widget.RelativeLayout;
-import android.widget.Toast;
+
 
 import androidx.appcompat.widget.Toolbar;
 
@@ -35,12 +41,12 @@ public class MainActivity extends AppCompatActivity {
     private SwipeRefreshLayout swipeRefresh;
     private RelativeLayout mainContainer;
     private Toolbar toolBar;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         DynamicColors.applyToActivityIfAvailable(this);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        webView = findViewById(R.id.webview);
         toolBar = (Toolbar) findViewById(R.id.toolBar);
         setSupportActionBar(toolBar);
         swipeRefresh = findViewById(R.id.swipeContainer);
@@ -48,14 +54,34 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onRefresh() {
                 webView.reload();
+            }
+        });
+        webView.setOnTouchListener(new View.OnTouchListener() {
+            GestureDetector gestureDetector = new GestureDetector(getApplicationContext(), new GestureDetector.SimpleOnGestureListener() {
+                @Override
+                public boolean onSingleTapUp(MotionEvent e) {
+                    /*if (readingMenu.getVisibility() == View.GONE ){
+                        readingMenu.setVisibility(View.VISIBLE);
+                    }
+                    else{
+                        readingMenu.setVisibility(View.GONE);
+                    }*/
+                return super.onSingleTapUp(e);
+                }
+            });
 
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+                if (checkUrl(webView.getUrl()))
+                {
+                gestureDetector.onTouchEvent(motionEvent);}
+                return false;
             }
         });
         loadWeb();
-
     }
 
-    public void loadWeb(){
+    public void loadWeb() {
         mainContainer = findViewById(R.id.mainContainer);
         webView = findViewById(R.id.webview);
         webView.getSettings().setDomStorageEnabled(true);
@@ -66,36 +92,37 @@ public class MainActivity extends AppCompatActivity {
         webView.setOverScrollMode(View.OVER_SCROLL_NEVER);
         swipeRefresh.setRefreshing(true);
         webView.setWebViewClient(new WebViewClient() {
-             @Override
-             public void onPageFinished(WebView view, String url) {
-                 injectCSS();
-                 super.onPageFinished(view, url);
-                 swipeRefresh.setRefreshing(false);
-                 showSystemBars();
-                 if (checkUrl(url)) {
-                     //immersive mode when at the reading page
-                     hideSystemBars();
-                     mainContainer.setFitsSystemWindows(false);
-                 }
-                 else{
+                 @Override
+                 public void onPageFinished(WebView view, String url) {
+                     injectCSS();
+                     super.onPageFinished(view, url);
+                     swipeRefresh.setRefreshing(false);
                      showSystemBars();
-                     mainContainer.setFitsSystemWindows(true);
+                     if (checkUrl(url)) {
+                         //immersive mode when at the reading page
+                         hideSystemBars();
+                         mainContainer.setFitsSystemWindows(false);
+                     } else {
+                         showSystemBars();
+                         mainContainer.setFitsSystemWindows(true);
+                     }
                  }
              }
-         }
         );
     }
+
     private boolean checkUrl(String url) {
         /*Checks if the web link is a reading page*/
         char slash = '/';
         int count = 0;
-        for (int i = 0; i < url.length(); i++){
+        for (int i = 0; i < url.length(); i++) {
             if (url.charAt(i) == slash) {
                 count++;
             }
         }
         return count == 8;
     }
+
     private void injectCSS() {
         try {
             InputStream inputStream = getAssets().open("style.css");
@@ -116,43 +143,44 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-   private void hideSystemBars(){
-       if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
-           getWindow().setDecorFitsSystemWindows(false);
-           WindowInsetsController controller = getWindow().getInsetsController();
-           if(controller != null) {
-               controller.hide(WindowInsets.Type.statusBars() | WindowInsets.Type.navigationBars());
-               controller.setSystemBarsBehavior(WindowInsetsController.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE);
-           }
-           getSupportActionBar().hide();
-       }
-       else{
-           getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                   | View.SYSTEM_UI_FLAG_FULLSCREEN
-                   | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-                   | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                   | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-                   | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
-          );}
+    private void hideSystemBars() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            getWindow().setDecorFitsSystemWindows(false);
+            WindowInsetsController controller = getWindow().getInsetsController();
+            if (controller != null) {
+                controller.hide(WindowInsets.Type.statusBars() | WindowInsets.Type.navigationBars());
+                controller.setSystemBarsBehavior(WindowInsetsController.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE);
+            }
+            getSupportActionBar().hide();
+        } else {
+            getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                    | View.SYSTEM_UI_FLAG_FULLSCREEN
+                    | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                    | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                    | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                    | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+            );
+        }
 
-       }
+    }
 
-   private void showSystemBars(){
-       if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
-           WindowInsetsController controller = getWindow().getInsetsController();
-           if (controller != null) {
-               controller.show(WindowInsetsCompat.Type.systemBars());
-           }
+    private void showSystemBars() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            WindowInsetsController controller = getWindow().getInsetsController();
+            if (controller != null) {
+                controller.show(WindowInsetsCompat.Type.systemBars());
+            }
             getSupportActionBar().show();
-       } else{
-           getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                   | View.SYSTEM_UI_FLAG_FULLSCREEN
-                   | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-                   | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                   | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-                   | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
-           }
-   }
+        } else {
+            getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                    | View.SYSTEM_UI_FLAG_FULLSCREEN
+                    | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                    | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                    | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                    | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+        }
+    }
+
     @Override
     public void onBackPressed() {
 
@@ -185,6 +213,7 @@ public class MainActivity extends AppCompatActivity {
         return true;
     }
 
+
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
         switch (item.getItemId()) {
@@ -197,9 +226,18 @@ public class MainActivity extends AppCompatActivity {
             case R.id.home:
                 webView.loadUrl("https://www.mangago.me/");
                 return true;
+            case R.id.history:
+                webView.loadUrl("https://www.mangago.me/home/history/");
+                return true;
         }
         return super.onOptionsItemSelected(item);
     }
 
 }
+
+
+
+
+
+
 
